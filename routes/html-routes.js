57a2e8d3e99fd5cbf08bac5e8e2bd3f1f47a236c
Mesut Sipar <mesutsipar@gmail.com>
2020-04-03 17:08:00 -0500

    adding changes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -31,6 +31,75 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
     }
     res.render("signup")
   });
+  
+  router.get("/addgroup", function(req, res){
+    res.render("addgroup")
+  });
+
+  router.get("/groupname", function(req, res){
+    res.render("groupname")
+  });
+
+
+  router.get("/groupadmin", function(req, res){
+    res.render("groupadmin")
+  });
+
+  router.get("/groupname", function(req, res){
+    res.render("groupname")
+  });
+
+
+  router.get("/members", function(req, res){
+    res.render("members")
+  });
+
+
+  router.get("/deletegroup", function(req, res){
+    res.render("deletegroup")
+  });
+
+
+  router.get("/updategroup", function(req, res){
+    res.render("updategroup")
+  });
+
+
+  router.get("/product", function(req, res){
+    res.render("product")
+  });
+
+  
+  router.get("/productowner", function(req, res){
+    res.render("productowner")
+  });
+
+
+  router.get("/productname", function(req, res){
+    res.render("productname")
+  });
+
+
+  router.get("/productcategory", function(req, res){
+    res.render("productcategory")
+  });
+
+
+  router.get("/productdescription", function(req, res){
+    res.render("productdescription")
+  });
+
+
+  router.get("/quantityinstock", function(req, res){
+    res.render("quantityinstock")
+  });
+
+
+  router.get("/logout", function(req, res){
+    res.render("logout")
+  });
+  
+
 
 
   // Here we've add our isAuthenticated middleware to this route.
